d25ea2a09bee7d29d9ed001e0a1b8bf833092681
Nuria Cano <alumno25dbt@gmail.com>
2025-05-08 17:58:31 +0000

    commit

--- models/staging/sql_server_dbo/stg_sql_server_dbo__orders.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__orders.sql
@@ -4,7 +4,10 @@ WITH cte12 AS (
 
 SELECT
     {{ dbt_utils.generate_surrogate_key(['order_id'])}} AS order_id,
-    shipping_service,
+    CASE 
+    WHEN status != 'preparing' THEN shipping_service 
+    ELSE 'por determinar'
+    END AS shipping_service,
     shipping_cost, 
     {{ dbt_utils.generate_surrogate_key(['address_id'])}} AS address_id,
     created_at,
@@ -12,11 +15,17 @@ SELECT
     WHEN promo_id != '' THEN promo_id
     ELSE 'Sin descuento'
     END AS desc_promo,
-    estimated_delivery_at,
+    CASE
+    WHEN status != 'preparing' THEN estimated_delivery_at
+    ELSE created_at + interval '3 days'
+    END AS estimated_delivery_at,
     order_cost,
     {{ dbt_utils.generate_surrogate_key(['user_id'])}} AS user_id,
     order_total,
-    delivered_at,
+    CASE
+    WHEN status != 'preparing' THEN delivered_at
+    ELSE '1999-01-01'
+    END AS delivered_at,
     {{ dbt_utils.generate_surrogate_key(['tracking_id'])}} AS tracking_id,
     status,
     _fivetran_deleted,
